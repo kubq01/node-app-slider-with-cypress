6abdd0aab06a249ed0491b70c1ff3930c94d75a3
kubq01 <kuba18062001@gmail.com>
2024-04-04 15:26:00 +0200

    1

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,31 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if buttons works', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'London');
+    cy.get('.swiper-button-prev').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks title and description', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide-active').find('p').should('contain', 'Italy');
+    cy.get('.swiper-slide-active').find('h1').should('contain', 'Rome');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').find('p').should('contain', 'United Kingdom');
+    cy.get('.swiper-slide-active').find('h1').should('contain', 'London');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').find('p').should('contain', 'France');
+    cy.get('.swiper-slide-active').find('h1').should('contain', 'Paris');
+  });
+});
